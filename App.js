da197904c3d185a119c27c3f1b7761c1a158ac7f
Diana Udisteanu <diana.udisteanu@yahoo.com>
2021-05-11 17:02:09 +0300

    Add dependency to firebase.

--- App.js
+++ App.js
@@ -6,6 +6,23 @@ import AddIntoStack from './routes/routes';
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZStcmIjQR96vY7l5r3xVkFDKQn0gpMSA",
+    authDomain: "monitoract-bc4fa.firebaseapp.com",
+    databaseURL: "https://monitoract-bc4fa-default-rtdb.firebaseio.com",
+    projectId: "monitoract-bc4fa",
+    storageBucket: "monitoract-bc4fa.appspot.com",
+    messagingSenderId: "366628099620",
+    appId: "1:366628099620:web:fd65ef4c156b26daaf6ab1"
+};
+
 const fontConfig = {
   "light-font": require("./fonts/ProximaNovaThin.otf"),
   "normal-font": require("./fonts/ProximaNovaRegular.otf"),
@@ -27,6 +44,8 @@ export default class App  extends React.Component {
 
   async componentDidMount(){
     this.loadFonts();
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
   }
 
   render(){
